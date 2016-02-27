c11177482508bd29a7c1381f8d18eab587ce60ac
iharis <charalampos.ioannou@gmail.com>
2016-02-28 00:03:29 +0200

    1st changes

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Hello World', servername:"Rasberry Pi" });
+  res.render('index', { title: "Hello World", servername:"Raspberry Pi"});
 });
 
 module.exports = router;
