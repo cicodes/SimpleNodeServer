325c54c782ccb78de18c665dc5852cce66fd4997
iharis <charalampos.ioannou@gmail.com>
2016-04-13 00:09:16 +0300

    6th Version of the serial port feature and added the speech recognition feature

---
+++ public/javascripts/speechRecognition.js
@@ -0,0 +1,77 @@
+var recognition = new webkitSpeechRecognition() || new SpeechRecognition();
+recognition.lang = "en-US";
+
+var recognizing = false;
+var ignore_onend;
+var final_transcript = '';
+
+recognition.continuous = true;
+recognition.interimResults = true;
+
+recognition.onstart = function() {
+    recognizing = true;
+    console.log('info_speak_now');
+};
+
+recognition.onerror = function(event) {
+    if (event.error == 'no-speech') {
+        console.log('info_no_speech');
+        ignore_onend = true;
+    }
+    if (event.error == 'audio-capture') {
+        console.log('info_no_microphone');
+        ignore_onend = true;
+    }
+    if (event.error == 'not-allowed') {
+        console.log('info_blocked');
+        ignore_onend = true;
+    }
+};
+
+recognition.onend = function() {
+    recognizing = false;
+    if (ignore_onend) {
+        return;
+    }
+
+    if (!final_transcript) {
+        console.log('info_start');
+        return;
+    }
+};
+
+recognition.onresult = function(event) {
+    var interim_transcript = '';
+    if (typeof(event.results) == 'undefined') {
+        recognition.onend = null;
+        recognition.stop();
+        upgrade();
+        return;
+    }
+
+    var result = '';
+    for (var i = event.resultIndex; i < event.results.length; ++i) {
+        if (event.results[i].isFinal) {
+            result = event.results[i][0].transcript;
+            break;
+        }
+
+    }
+    if(result != "")    controlWithSpeech(result);
+};
+
+function controlWithSpeech(result){
+    console.log(result + "\n");
+    var houseName = "Jarvis";
+    if(result.indexOf(houseName)> -1){
+        if(result.indexOf("open")> -1){
+            if(result.indexOf("all lights")> -1){
+                return;
+            }
+        }else if(result.indexOf("close")> -1){
+            if(result.indexOf("all lights")> -1){
+                return;
+            }
+        }
+    }
+}
